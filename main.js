ec73b9670d00f1948ccb79726ffc3984268580d5
elina singh <elinasingh1997@gmail.com>
2017-07-20 21:24:22 +0530

    working in controllers

---
+++ main.js
@@ -0,0 +1,11 @@
+
+		 var foodieApp = angular.module('foodieApp',[]);
+		 
+		 
+		  foodieApp.controller('mainController',function($scope) {
+
+	      })
+
+			foodieApp.controller('mainController',function($scope) {
+				$scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+			})

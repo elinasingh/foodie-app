b9575d4d550b415478c6cf34542b676d0f4e3173
elinasingh <elinasingh1997@gmail.com>
2017-08-02 19:07:52 +0530

    add new file for order and changes in other files

--- main.js
+++ main.js
@@ -8,21 +8,25 @@
 			 foodieApp.config(function ($routeProvider) { //pass function as parameter
 				$routeProvider
 				.when('/',{
-					templateUrl: 'pages/login.html',
+					templateUrl: 'pages/login.html',    //login url
 					controller: 'loginController'
 				})
 				.when('/home',{
-					templateUrl: 'pages/main.html',
+					templateUrl: 'pages/main.html',    //main url
 					controller: 'mainController'
 				})
 				.when('/restaurant/:id', {
-		      templateUrl: 'pages/restaurant.html',
+		      templateUrl: 'pages/restaurant.html',   //restaurant url
 		      controller: 'restaurantController'
 	       })
 				 .when('/fav', {
-						templateUrl: 'pages/fav.html',
+						templateUrl: 'pages/fav.html',       //fav url
 						controller: 'favController'
 					})
+					.when('/order', {
+						 templateUrl: 'pages/order.html',       //order url
+						 controller: 'orderController'
+					 })
 			})
 
 
@@ -33,7 +37,7 @@
 						 $scope.ingredients = [];
 							console.log($routeParams.id);
 							$scope.restaurantId = $routeParams.id;
-							var restaurants = [{ //restaurant 1
+							var restaurants = [{      //restaurant 1
 								name: 'Ricos',
 								address: '2526, 1st Floor, Hudson Lane, Kingsway Camp, Delhi',
 								location: 'Delhi University-GTB Nagar',
@@ -52,7 +56,7 @@
 								order:'90mins Rs.20',
 								image: 'https://tinyurl.com/ybsnhfna'
 							},
-							{    //restaurant 2
+							{                           //restaurant 2
 								name: 'Hakuna Matata',
 								address: '1231/35/2,7th Phase, Opposite Brigade, Bangalore',
 								location: 'JP Nagar',
@@ -62,12 +66,16 @@
 								cost: '1200',
 								id:  2,
 								hours: '12:30 AM to 11:30 PM (Mon-Sun)',
+								bestDish: {
+										name: 'Noodles',
+										image: 'http://freedesignfile.com/upload/2016/12/Tasty-Chinese-Food-HD-picture-03.jpg'
+									},
 								votes:'200',
 								reviews:'300',
 								order:'60mins Rs.80',
 								image: 'https://tinyurl.com/y9tebdor'
 							},
-							{   //restaurant 3
+							{                               //restaurant 3
 								name: 'Love & Cheesecake',
 								address: 'Eden 2, Central Ave, Hiranandani Gardens, Powai, Mumbai',
 								location: 'Powai',
@@ -77,13 +85,17 @@
 								cost: '800',
 								id:  3,
 								hours: '9 AM to 12 Midnight (Mon-Sun)',
+								bestDish: {
+										name: 'pie cake',
+										image: 'http://data.freehdw.com/multicolor-food-donuts-hd-background.jpg'
+									},
 								votes:'190',
 								reviews:'250',
 								order:'50mins Rs.70',
 								discount:'Get 15% off on all home-delivery orders. Valid only when you order online on Zomato.',
 								image: 'https://tinyurl.com/y7hwh5mr'
 							},
-							{   //restaurant 4
+							{                                 //restaurant 4
 								name: 'Tribute',
 								address: '89-B, Monica Complex, Ambamata Road, Udaipur',
 								location: 'Fateh Sagar',
@@ -93,12 +105,16 @@
 								cost: '1500',
 								id:  4,
 								hours: '11:30 AM to 11 PM (Mon-Sun)',
+								bestDish: {
+										name: 'stake',
+										image: 'http://photo.elsoar.com/wp-content/images/Meat-And-Vegetable-Salad-Wallpaper-Hd.jpg'
+									},
 								votes:'150',
 								reviews:'280',
 								order:  '20mins Rs.100',
 								image: 'https://tinyurl.com/y9daexb6'
 							},
-							{   //restaurant 5
+							{                                 //restaurant 5
 								name: 'Farzi Cafe',
 								address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 								location: 'Connaught Place',
@@ -108,12 +124,16 @@
 								cost: '2200',
 								id:  5,
 								hours: '12 Noon to 1 AM (Mon-Sun)',
+								bestDish: {
+										name: 'Burger',
+										image: 'http://cdn.wallpapersafari.com/93/11/2g9Mch.jpg'
+									},
 								votes:'280',
 								reviews:'400',
 								order:'70mins Rs.30',
 								image: 'https://tinyurl.com/y9urntqf'
 							},
-							{   //restaurant 6
+							{                                   //restaurant 6
 								name: 'Jalpaan Restraunt',
 								address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 								location: 'RS Puram',
@@ -123,13 +143,17 @@
 								cost: '1200',
 								id:  6,
 								hours: '12 Noon to 10:30 PM (Mon-Sun)',
+								bestDish: {
+										name: 'Mexicon food',
+										image: 'http://cdn.wallpapersafari.com/0/57/sq0MY5.jpg'
+									},
 								votes:'120',
 								reviews:'300',
 								order:'90mins Rs.80',
 								image: 'https://tinyurl.com/ycp69uuf'
 							},
 
-							{   //restaurant 7
+							{                               //restaurant 7
 								name: 'The French Door',
 								address: '102, West Bashyakaralu Road, RS Puram, Coimbatore',
 								location: 'RS Puram',
@@ -139,12 +163,16 @@
 								cost: '1800',
 								id:  7,
 								hours: '11 AM to 11 PM (Mon-Sun)',
+								bestDish: {
+										name: 'Donates',
+										image: 'https://foodanddrinkshdwallpapers.files.wordpress.com/2014/10/french_breakfast.jpg'
+									},
 								votes:'180',
 								reviews:'600',
 								order:'70mins Rs.50',
 								image: 'https://tinyurl.com/yb5fn87q'
 							},
-							{  //restaurant 8
+							{                                  //restaurant 8
 								name: 'Bikaner Sweets',
 								address: 'Near Devkamal Hospital, Ratu Road, Ratu, Ranch',
 								location: 'Ratu',
@@ -154,6 +182,10 @@
 								cost: '200',
 								id:  8,
 								hours: ' 9 AM to 9:30 PM (Mon-Sun)',
+								bestDish: {
+										name: 'Sweets',
+										image: 'http://bsnscb.com/data/out/191/38899161-sweets-wallpapers.jpg'
+									},
 								votes:'170',
 								reviews:'500',
 								order:'50mins Rs.80',
@@ -200,7 +232,7 @@
 
 							}
 
-										$scope.goToFav = function() {
+										$scope.goToFav = function() {       //goto fav function
 												$location.url('fav')
 												console.log($location.url);
 											}
@@ -253,7 +285,8 @@ foodieApp.controller('favController',function($scope,$location,$http) {
                     }]
 
 
-         //list for you may like
+
+         //list1 for you may like OR NOT
 						$scope.lists1 = [
 						{'vl' : 'vegetable'},
 						{'vl' : 'almond'},
@@ -270,7 +303,7 @@ foodieApp.controller('favController',function($scope,$location,$http) {
 								}
 					};
 
-					//list for you may not like
+					//list2 for you may LIKE OR NOT like
 					$scope.lists2 = [
 					{'vl' : 'meat'},
 					{'vl' : 'egg'},
@@ -290,16 +323,16 @@ foodieApp.controller('favController',function($scope,$location,$http) {
       //calling the getfav function
 				$scope.getFav = function(url) {
 						var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
-										$http({
+										$http({                                                        //api call
 											'method': 'POST',
 											'url': 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs',
 											'headers': {
-												'Authorization': 'Key fe486afe04fe4bf9a1fec63b55d9f8cb',
+												'Authorization': 'Key fe486afe04fe4bf9a1fec63b55d9f8cb',              //api key
 												'Content-Type': 'application/json'
 											},
 											'data': data,
 
-										}).then(function (response) {
+										}).then(function (response) {     //fav food helper code
 													var ingredients = response.data.outputs[0].data.concepts;
 										  			var list = '';
 														//  var cboxArray = [];
@@ -357,6 +390,11 @@ foodieApp.controller('favController',function($scope,$location,$http) {
 											 $location.url('home');
 										 }
 			          })
+								foodieApp.controller('orderController',function($scope,$location) {
+
+													console.log("Do Something")
+
+									 })
 
 		      //iss function ke andar jo b aayega bhi kaam hoga maincontroller ka
 
